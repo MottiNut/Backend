3871e01c01efac6e5ff50dd0724225fec438a1ed
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2025-06-08 00:20:14 -0500

    initial commit

---
+++ mottinut/src/main/java/com/mottinut/MottinutApplication.java
@@ -0,0 +1,13 @@
+package com.mottinut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MottinutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MottinutApplication.class, args);
+	}
+
+}
